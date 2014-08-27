2f019a45fd93933c7cd915357df227b5e47efebc
Greg Woodcock <greg@yoomee.com>
2014-08-27 14:40:15 +0100

    Dont compress assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,10 @@ Rails.application.configure do
   config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.compress = true
+  config.assets.js_compressor = NoCompression.new
+  config.assets.css_compressor = NoCompression.new
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
